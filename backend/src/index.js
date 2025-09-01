aebaf3ccde5bdb209db445dab3c158e780798cca
Shubhankar <shubhankar.sharma97@gmail.com>
2025-09-01 14:36:58 +0530

    Ready to Deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors"
-
+import path from "path"
 import notesRouter from "./routes/notes.routes.js";
 import { connectDB } from "../config/db.js";
 import redis from "../config/redis.server.js";
@@ -15,13 +15,26 @@ redis.on("connect",()=>{
 //disabling tips in console 
 dotenv.config({ quiet: true });
 const app=express();
-
+const __dirname=path.resolve();
 app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:5173"
-}));
+
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors({
+        origin:"http://localhost:5173"
+    }));
+    
+}
 app.use(rateLimiter(100,200));
 app.use("/api/notes",notesRouter);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
+
 
 
 connectDB().then(()=>{
