d906c53063d46e658530157026b7a0facd3e2551
Shubhankar <shubhankar.sharma97@gmail.com>
2025-09-01 14:52:18 +0530

    comment *get

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,9 +30,9 @@ app.use("/api/notes",notesRouter);
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
-    })
+    // app.get("*",(req,res)=>{
+    //     res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    // })
 }
 
 
